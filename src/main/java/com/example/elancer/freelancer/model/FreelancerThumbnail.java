7c0fa25b1c65c8455a5913a01564260fa37175ea
mins1031 <urisegea@naver.com>
2022-07-22 12:47:14 +0900

    feat: 섬네일 컬럼 용량 확대

--- src/main/java/com/example/elancer/freelancer/model/FreelancerThumbnail.java
+++ src/main/java/com/example/elancer/freelancer/model/FreelancerThumbnail.java
@@ -4,6 +4,7 @@ import com.example.elancer.common.model.BasicEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,12 +13,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FreelancerThumbnail extends BasicEntity {
     @NotNull
+    @Size(max = 1500)
     private String thumbnailPath;
 
     @OneToOne(fetch = FetchType.LAZY)
